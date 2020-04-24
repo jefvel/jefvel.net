571c33848954bc533e7dae1c973e42af3e8dd8a0
jefvel <geekku@gmail.com>
2020-04-24 18:18:00 +0200

    adsf

--- commit-gh-pages-files.sh
+++ commit-gh-pages-files.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+
+
 if [ "`git status -s`" ]
 then
       echo "The working directory is dirty. Please commit any pending changes."
@@ -13,7 +15,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -26,4 +28,3 @@ cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)
 
 echo "Pushing to github"
 git push --all
-
